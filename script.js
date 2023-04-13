acdad5e1594f7172d05259c5a56ff08454af0a24
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2023-04-13 21:14:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function fun(){
+    window.open("about.html","_self");
+}
+function back(){
+    window.open("index.html","_self");
+}
+function funst(){
+    window.open("fun-street.html","_self");
+}
+function djevent(){
+    window.open("dj.html","_self");
+}
+function concert(){
+    window.open("concert.html","_self");
+}
+function register(){
+    window.open("register.html");
+}
